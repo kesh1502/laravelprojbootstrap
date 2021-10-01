3435987cb425cf8ba3f776a133359d414b01edf2
kesh1502 <jootunkeshav@gmail.com>
2021-10-01 16:00:08 +0400

    menu button change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -118,7 +118,7 @@
             </div>
             
             <!-- toggler -->
-            <button class="btn float-end" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" role="button">Menu
+            <button class="btn btn-dark" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" role="button">Menu
             </button>
             <button class="navbar-toggler"  type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
